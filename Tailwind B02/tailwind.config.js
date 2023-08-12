2acf0978c851f468036461f7597c130111c626d1
Nikhil Borse <nikhilrb13@gmail.com>
2023-08-12 16:29:48 +0530

    tailwind css : gallery images

---
+++ Tailwind B02/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./direction/**/*.{html,js}"],
+  // content: ["./direction/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
